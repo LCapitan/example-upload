fa0388d70e87a6c93a60d41a207eaddbd7aedfdd
Scott J. Vaverchak <Scott.Vaverchak@gmail.com>
2017-09-14 16:14:28 -0400

    Adding missing files

---
+++ MeemicMobileApp/Views/MyMeemic/Claims/AutoClaimStepTwoView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MeemicMobileApp.Views.MyMeemic.Claims
+{
+    public partial class AutoClaimStepTwoView : ContentPage
+    {
+        public AutoClaimStepTwoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
